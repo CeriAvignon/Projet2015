697eb5edad0df64f2896dd1b1809a807bdffe0c6
Zakaria LAHRACH <zakaria.lahrach@gmail.com>
2015-12-01 15:53:57 +0100

    hgsdg

---
+++ src/fr/univavignon/courbes/inter/groupe12/error_display.java
@@ -0,0 +1,17 @@
+package fr.univavignon.courbes.inter.groupe12;
+
+import fr.univavignon.courbes.inter.ErrorHandler;
+
+/**
+ * @author uapv1601921
+ *
+ */
+public class error_display implements ErrorHandler {
+
+	@Override
+	public void displayError(String errorMessage) {
+		
+
+	}
+
+}
