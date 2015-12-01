ac53778df2a96a304668db5f38c14404e8cc0458
Maxime DAPP <maxime.dapp@yahoo.fr>
2015-12-01 15:44:11 +0100

    [ADDED] Client.java 	[ADDED] All functions from ClientCommunication.java
    [ADDED] Server.java
    	[ADDED] All functions from ServerCommunication.java

---
+++ src/fr/univavignon/courbes/network/groupe15/Client.java
@@ -0,0 +1,91 @@
+package fr.univavignon.courbes.network.groupe15;
+
+import java.util.List;
+import java.util.Map;
+
+import fr.univavignon.courbes.common.Board;
+import fr.univavignon.courbes.common.Direction;
+import fr.univavignon.courbes.common.Profile;
+import fr.univavignon.courbes.network.ClientCommunication;
+
+/**
+ * 
+ * @author uapv1504059
+ *
+ */
+
+public class Client implements ClientCommunication { 
+
+	@Override
+	public String getIp() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setIp(String ip) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getPort() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setPort(int port) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void launchClient() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void closeClient() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Profile> retrieveProfiles() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer retrievePointThreshold() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Board retrieveBoard() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void sendCommands(Map<Integer, Direction> commands) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String retrieveText() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void sendText(String message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/fr/univavignon/courbes/network/groupe15/Server.java
@@ -0,0 +1,67 @@
+package fr.univavignon.courbes.network.groupe15;
+
+import java.util.List;
+import java.util.Map;
+
+import fr.univavignon.courbes.common.Board;
+import fr.univavignon.courbes.common.Direction;
+import fr.univavignon.courbes.common.Profile;
+import fr.univavignon.courbes.network.ServerCommunication;
+
+/**
+ * 
+ * @author uapv1504059
+ *
+ */
+
+public class Server implements ServerCommunication {
+
+	@Override
+	public void launchServer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void closeServer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sendPlayers(List<Profile> profiles) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sendPointThreshold(int pointThreshold) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sendBoard(Board board) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Map<Integer, Direction> retrieveCommands() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void sendText(String message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String[] retrieveText() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
