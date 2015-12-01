5812e81644a6a01382d5d28dd1085f1c6e2a6bc5
Antoine Letourneur <ant984@free.fr>
2015-12-01 17:21:13 +0100

    creation package groupe18 + class

---
+++ src/fr/univavignon/courbes/graphics/groupe18/GraphicDisplayGroupe18.java
@@ -0,0 +1,38 @@
+package fr.univavignon.courbes.graphics.groupe18;
+
+import java.util.List;
+
+import fr.univavignon.courbes.common.Board;
+import fr.univavignon.courbes.common.Profile;
+import fr.univavignon.courbes.graphics.GraphicDisplay;
+
+/**
+ * @author uapv1504323
+ *
+ */
+public class GraphicDisplayGroupe18 implements GraphicDisplay {
+
+	@Override
+	public void init(Board board, int pointThreshold, List<Profile> players) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void end() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
