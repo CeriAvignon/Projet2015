cf2eb013dc104f2dc09a36e30a71ee81daa7cbd9
Charlie Brugvin <charlie.brugvin@gmail.com>
2015-12-01 15:51:35 +0100

    remise a l'etat 'master' des fichiers source + ajout du package 'groupe10' contenant la copie des fichiers source concerné

--- src/fr/univavignon/courbes/common/Position.java
+++ src/fr/univavignon/courbes/common/Position.java
@@ -14,22 +14,4 @@ public class Position implements Serializable
 	public int x;
 	/** Position sur l'axe des ordonnées */
 	public int y;
-	
-	public Position(int x, int y)
-	{
-		this.x = x; 
-		this.y = y;
-	}
-	
-	public int getX()
-	{
-		return x;
-	}
-	
-	public int getY()
-	{
-		return y;
-	}
-
-
 }
