9af38f7cc961cfb847aa4267078020dc5928677e
Rudy Bardout <rudybardout@orange.fr>
2015-12-01 16:23:48 +0100

    Ajout de toutes les classes

---
+++ src/fr/univavignon/courbes/physics/groupe07/Position.java
@@ -0,0 +1,17 @@
+package fr.univavignon.courbes.graphics.groupe07;
+
+import java.io.Serializable;
+
+/**
+ * Cette classe permet de représenter la position d'un
+ * pixel dans un repère à deux dimensions.
+ */
+public class Position implements Serializable
+{	/** Numéro de série (pour {@code Serializable}) */
+	private static final long serialVersionUID = 1L;
+	
+	/** Position sur l'axe des abscisses */
+	public int x;
+	/** Position sur l'axe des ordonnées */
+	public int y;
+}
