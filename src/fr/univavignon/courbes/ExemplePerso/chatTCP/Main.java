a990d313fbdd7779c1d707f2e4b9a94bb3b13e5a
Kemacian <loic.toro@free.fr>
2015-12-05 18:38:29 +0100

    obtenir ip machine

--- src/fr/univavignon/courbes/ExemplePerso/chatTCP/Main.java
+++ src/fr/univavignon/courbes/ExemplePerso/chatTCP/Main.java
@@ -14,6 +14,31 @@ public class Main {
 	      
 	         Thread t = new Thread(new clientTCP(host, port));
 	         t.start();
+	         
+	         /*String ip = InetAddress.getLocalHost().getHostAddress().toString();
+		 		System.out.println("Votre IP est : " + ip);*/ // méthode A (obtenir ip locale)
+		 	  
+		         //méthode B (marche tout le temps !)
+		         /*String ip;
+		 	    try {
+		 	        Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+		 	        while (interfaces.hasMoreElements()) {
+		 	            NetworkInterface iface = interfaces.nextElement();
+		 	            // filters out 127.0.0.1 and inactive interfaces
+		 	            if (iface.isLoopback() || !iface.isUp())
+		 	                continue;
+
+		 	            Enumeration<InetAddress> addresses = iface.getInetAddresses();
+		 	            while(addresses.hasMoreElements()) {
+		 	                InetAddress addr = addresses.nextElement();
+		 	                ip = addr.getHostAddress();
+		 	                if (ip.startsWith("192.168."))
+		 	                System.out.println(iface.getDisplayName() + " " + ip);
+		 	            }
+		 	        }
+		 	    } catch (SocketException e) {
+		 	        throw new RuntimeException(e);
+		 	    }*/
 	}
 
 }
