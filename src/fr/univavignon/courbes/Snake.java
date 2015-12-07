329c20e45aa21ac9f82f481528f7848492f79920
Castillo Quentin <quentin.castillo@hotmail.fr>
2015-12-07 16:15:15 +0100

    Test
    work in progress...

--- src/fr/univavignon/courbes/Snake.java
+++ src/fr/univavignon/courbes/Snake.java
@@ -1,128 +1,53 @@
-package fr.univavignon.courbes;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.List;
+package fr.univavignon.courbes.common;
 
-// Shift + ctrl + o     to import automaticly 
-public class Snake 
-{
-	private int idPlayer;
-	private Position head; 			//Position = (x,y)
-	private double direction; 		//angle
-	private double ray;				//Size
-	private double speed;
-	private boolean state;			// 0 = dead 	   ----- 1 = alive
-	private boolean crashmode;		// 0 = nocollision ----- 1 = collision   
-	private boolean invertmode; 	// 0 = normal keys ----- 1 = invert keys
-	private double holerate;		// % hole apparition
-	private boolean planemode; 		// 0 = not plane   ----- 1 = plane mode
-	private List changes;     		// List of Items
+import java.io.Serializable;
+import java.util.Map;
 
+/**
+ * Cette classe contient les informations et caractéristiques d'un snake.
+ * <br/>
+ * L'ID du joueur associé au snake est relatif à la partie courante. Par 
+ * exemple, si on a 4 joueurs, alors leurs ID vont de 0 à 3. À ne pas confondre
+ * avec l'ID du profile, qui est valable pour le jeu globalement, et indépendant
+ * de toute partie.
+ */
+public class Snake implements Serializable
+{	/** Numéro de série (pour {@code Serializable}) */
+	private static final long serialVersionUID = 1L;
 	
-	public Snake(int id)
-	{
-		idPlayer 	= id;
-		head 		= new Position();
-		direction   = -90.0; 		// vers le haut
-		ray 		= 3.0;  		// pixel ?
-		speed 		= 1.0;			// pixel/frame ?
-		state 		= true;
-		crashmode 	= true;
-		invertmode  = false;
-		holerate 	= 5.0;			// 5% ?
-		planemode  = false;
-		changes = new ArrayList();
-	}
+	/** Numéro du joueur associé à ce snake, dans la partie en cours */
+	public int playerId;
+	/** Numéro du profil associé à ce snake, dans la BD du jeu */
+	public int profileId;
 	
-	/***************************************************************************************************************************************************************\
-	/																																							   *\
-	/																		GETTERS & SETTERS																	   *\
-	/																																							   *\
-	/***************************************************************************************************************************************************************/
-	public int getID()
-	{
-		return idPlayer;
-	}
+	/** Position courante de la tête du Snake en abscisse */
+	public int currentX;
+	/** Position courante de la tête du Snake en ordonnée */
+	public int currentY;
 	
-	public boolean isAlive()
-	{
-		return state;
-	}
+	/** Angle représentant la direction de déplacement courante du snake */
+	public double currentAngle;
+	/** Rayon de la tête du snake */
+	public double headRadius;
+	/** Vitesse du snake, exprimée en pixel par ms */
+	public double currentSpeed;
 	
-	public boolean isCrashMode()
-	{
-		return crashmode;
-	}
+	/** Etat du snake : {@code false} il est mort, {@code true} il est vivant */
+	public boolean state;
+	/** Mode de collision : {@code false} il est insensible aux collisions, {@code true} il l’est */
+	public boolean collision;
+	/**Inversion des commandes : {@code false} le snake est dirigé normalement, {@code true} ses commandes sont inversées */
+	public boolean inversion;
 	
-	public boolean isInversed()
-	{
-		return invertmode;
-	}
+	/** Taux de création de trous dans le tracé : pour 0 on n'a aucun trou, pour 1 on n'a aucune traînée (i.e. que des trous) */
+	public double holeRate;
 	
-	public boolean isFlying()
-	{
-		return planemode;
-	}
+	/** Mode avion ({@code true}) ou pas ({@code false}) */
+	public boolean fly;
 	
-	public double getSpeed()
-	{
-		return speed;
-	}
+	/** Items affectant ce snake, associés à leur temps d'effet restant (en ms) */
+	public Map<Item, Long> currentItems;
 	
-	public double rateHoles()
-	{
-		return holerate;
-	}
-	
-	public double getSize()
-	{
-		return ray;
-	}
-	
-	public Position getPosition()
-	{
-		return head;
-	}
-	
-	/***************************************************************************************************************************************************************\
-	/																																							   *\
-	/																		ACTIONS																				   *\
-	/																																							   *\
-	/***************************************************************************************************************************************************************/
-
-	
-	public void turnLeft()
-	{
-		direction--;
-	}
-	
-	public void turnRight()
-	{
-		direction++;
-	}
-	
-	public void newChange(Changes object)
-	{
-		changes.add(object);
-	}
-	
-	public void getChanges()
-	{
-		if(changes.size() <= 0)
-		{
-			System.out.println("No changes for now");
-		}
-		else
-		{
-			for(int i = 0; i < changes.size(); i++) {   
-				System.out.println("something");
-			} 			
-		}
-
-	}
+	/** Score courant du joueur associé au snake dans la partie courante */
+	public int currentScore;
 }
-
-
-	
-//double axeX1=cos(angle1*PI/180.0);
-//double axeY1=sin(angle1*PI/180.0);
---
+++ src/fr/univavignon/courbes/common/Round.java
@@ -0,0 +1,14 @@
+package fr.univavignon.courbes;
+
+public class Round 
+{
+	private Board game;
+	private Item item;
+	
+	
+	public Round()
+	{
+		game = new Board();
+		item = new Item();
+	}
+}
