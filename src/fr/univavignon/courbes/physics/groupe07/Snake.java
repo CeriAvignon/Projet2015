669c2bcee9e017e2be1438cfc89a8deaa6efa1f8
Laurent Harkiolakis <laurent.harkiolakis@alumni.univ-avignon.fr>
2015-12-07 16:54:17 +0100

    constructeur snake

--- src/fr/univavignon/courbes/physics/groupe07/Snake.java
+++ src/fr/univavignon/courbes/physics/groupe07/Snake.java
@@ -12,42 +12,65 @@ import java.util.Map;
  * de toute partie.
  */
 public class Snake implements Serializable
-{	/** Numéro de série (pour {@code Serializable}) */
-	private static final long serialVersionUID = 1L;
-	
-	/** Numéro du joueur associé à ce snake, dans la partie en cours */
-	public int playerId;
-	/** Numéro du profil associé à ce snake, dans la BD du jeu */
-	public int profileId;
-	
-	/** Position courante de la tête du Snake en abscisse */
-	public int currentX;
-	/** Position courante de la tête du Snake en ordonnée */
-	public int currentY;
-	
-	/** Angle représentant la direction de déplacement courante du snake */
-	public double currentAngle;
-	/** Rayon de la tête du snake */
-	public double headRadius;
-	/** Vitesse du snake, exprimée en pixel par ms */
-	public double currentSpeed;
-	
-	/** Etat du snake : {@code false} il est mort, {@code true} il est vivant */
-	public boolean state;
-	/** Mode de collision : {@code false} il est insensible aux collisions, {@code true} il l’est */
-	public boolean collision;
-	/**Inversion des commandes : {@code false} le snake est dirigé normalement, {@code true} ses commandes sont inversées */
-	public boolean inversion;
-	
-	/** Taux de création de trous dans le tracé : pour 0 on n'a aucun trou, pour 1 on n'a aucune traînée (i.e. que des trous) */
-	public double holeRate;
-	
-	/** Mode avion ({@code true}) ou pas ({@code false}) */
-	public boolean fly;
-	
-	/** Items affectant ce snake, associés à leur temps d'effet restant (en ms) */
-	public Map<Item, Long> currentItems;
-	
-	/** Score courant du joueur associé au snake dans la partie courante */
-	public int currentScore;
+{        /** Numéro de série (pour {@code Serializable}) */
+        private static final long serialVersionUID = 1L;
+        
+        /** Numéro du joueur associé à ce snake, dans la partie en cours */
+        public int playerId;
+        /** Numéro du profil associé à ce snake, dans la BD du jeu */
+        public int profileId;
+        
+        /** Position courante de la tête du Snake en abscisse */
+        public int currentX;
+        /** Position courante de la tête du Snake en ordonnée */
+        public int currentY;
+        
+        /** Angle représentant la direction de déplacement courante du snake */
+        public double currentAngle;
+        /** Rayon de la tête du snake */
+        public double headRadius;
+        /** Vitesse de déplacement du snake, exprimée en pixel par ms */
+    	public double movingSpeed;
+    	/** Vitesse à laquelle le snake change de direction, exprimée en radians par ms */
+    	public double turningSpeed;
+        
+        /** Etat du snake : {@code false} il est mort, {@code true} il est vivant */
+        public boolean state;
+        /** Mode de collision : {@code false} il est insensible aux collisions, {@code true} il l’est */
+        public boolean collision;
+        /**Inversion des commandes : {@code false} le snake est dirigé normalement, {@code true} ses commandes sont inversées */
+        public boolean inversion;
+        
+        /** Taux de création de trous dans le tracé : pour 0 on n'a aucun trou, pour 1 on n'a aucune traînée (i.e. que des trous) */
+        public double holeRate;
+        
+        /** Mode avion ({@code true}) ou pas ({@code false}) */
+        public boolean fly;
+        
+        /** Items affectant ce snake, associés à leur temps d'effet restant (en ms) */
+        public Map<Item, Long> currentItems;
+        
+        /** Score courant du joueur associé au snake dans la partie courante */
+        public int currentScore;
+        
+        public Snake(int id,int numberId, int abscisse, int ordonnee, double direction, double sizeHead, double pace, double turn, boolean alive, boolean invicible, boolean reverse, double cheese, boolean bird, int score)
+        {
+                this.playerId = id;
+                this.profileId = numberId;
+                this.currentX = abscisse;
+                this.currentY = ordonnee;
+                this.currentAngle = direction;
+                this.headRadius = sizeHead;
+                this.movingSpeed = pace;
+                this.turningSpeed = turn;
+                this.state = alive;
+                this.collision = invicible;
+                this.inversion = reverse;
+                this.holeRate = cheese;
+                this.fly = bird;
+                this.currentScore = score;
+                
+                
+                
+        }
 }
