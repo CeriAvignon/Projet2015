d35f5e5a2a7e3d1c9d99be37c5fb3207f090fdcd
Omar Sy <omarsysy@gmail.com>
2015-12-07 16:49:09 +0100

    ss

--- src/fr/univavignon/courbes/graphics/groupe23/MyGraphic.java
+++ src/fr/univavignon/courbes/graphics/groupe23/MyGraphic.java
@@ -1,17 +1,24 @@
 package fr.univavignon.courbes.graphics.groupe23;
 
+import java.awt.Color;
 import java.util.List;
+
+import javax.swing.JList;
 
 import fr.univavignon.courbes.common.Board;
 import fr.univavignon.courbes.common.Profile;
 import fr.univavignon.courbes.graphics.GraphicDisplay;
 
 public class MyGraphic implements GraphicDisplay {
-
+Board b;
+Panel panel= new Panel();
+JList list=new JList();
 	@Override
 	public void init(Board board, int pointThreshold, List<Profile> players) {
 		// TODO Auto-generated method stub
-		
+		this.b=board;
+		panel.setBackground(Color.BLACK);
+		panel.setBounds(100, 49,b.width , b.height);
 	}
 
 	@Override
