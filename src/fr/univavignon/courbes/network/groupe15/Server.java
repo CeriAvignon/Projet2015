7d8b719951528d85b6377cbeea6382386b515a80
Tony Di Malta <Sky-Angels8@hotmail.fr>
2015-12-07 16:21:53 +0100

    Test Client/Server

--- src/fr/univavignon/courbes/network/groupe15/Server.java
+++ src/fr/univavignon/courbes/network/groupe15/Server.java
@@ -1,5 +1,8 @@
 package fr.univavignon.courbes.network.groupe15;
 
+import java.net.*;
+import java.io.*;
+
 import java.util.List;
 import java.util.Map;
 
@@ -15,6 +18,48 @@ import fr.univavignon.courbes.network.ServerCommunication;
  */
 
 public class Server implements ServerCommunication {
+
+	/**
+	 * @param args Port de connexion
+	 * @throws IOException Gestion des exceptions
+	 */
+	public static void main(String[] args) throws IOException {
+        
+        if (args.length != 1) {
+            System.err.println("Usage: java KnockKnockServer <port number>");
+            System.exit(1);
+        }
+
+        int portNumber = Integer.parseInt(args[0]);
+
+        try ( 
+            ServerSocket serverSocket = new ServerSocket(portNumber);
+            Socket clientSocket = serverSocket.accept();
+            PrintWriter out =
+                new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(clientSocket.getInputStream()));
+        ) {
+        
+            String inputLine, outputLine;
+            
+            // Initiate conversation with client
+            Client kkp = new Client();
+            outputLine = kkp.processInput(null);
+            out.println(outputLine);
+
+            while ((inputLine = in.readLine()) != null) {
+                outputLine = kkp.processInput(inputLine);
+                out.println(outputLine);
+                if (outputLine.equals("Bye."))
+                    break;
+            }
+        } catch (IOException e) {
+            System.out.println("Exception caught when trying to listen on port "
+                + portNumber + " or listening for a connection");
+            System.out.println(e.getMessage());
+        }
+    }
 
 	@Override
 	public void launchServer() {
