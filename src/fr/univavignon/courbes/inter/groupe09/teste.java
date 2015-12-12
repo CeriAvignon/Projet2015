4c8e8d3cda2edfd8b2b88313faefeeaefef84c4b
Kouseila Messas <k_messas@yahoo.fr>
2015-12-12 16:41:46 +0100

    classe test

--- src/fr/univavignon/courbes/inter/groupe09/teste.java
+++ src/fr/univavignon/courbes/inter/groupe09/teste.java
@@ -1,16 +1,22 @@
 package fr.univavignon.courbes.inter.groupe09;
+import java.awt.*;
+import javax.swing.*;
 
+/**
+ * @author groupe09
+ *
+ */
 public class teste {
 
 	/**
-	 * @param args
+	 * @param args argument de la fonction main 
 	 */
 	public static void main(String[] args) {
-
-		Error r1 = new Error(); 
-		r1.displayError("Connexion interompu");
-		
-		
+	// teste de la methode affichage des erreur
+	Error r1 = new Error(); 
+	r1.displayError("Connexion interompu");
+	// teste des menu du jeu
+	Bienvenu m1 = new Bienvenu("Atchung Die Kurv", 250, 300);		
 	}
 
 }
