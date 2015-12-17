386138bbb1c653ad2d4044600f6c51dafceed15d
Laurent Harkiolakis <laurent.harkiolakis@alumni.univ-avignon.fr>
2015-12-17 12:32:16 +0100

    commandes

--- src/fr/univavignon/courbes/physics/groupe07/Main.java
+++ src/fr/univavignon/courbes/physics/groupe07/Main.java
@@ -17,9 +17,13 @@ public class Main
 	public static void main(String[] args)
 	
 	{
-		int idPlayers[] = {0,1,2,3,4,5};
+		int idPlayers[] = {0,1,2,3};
 		Round newRound = new Round(500,500,idPlayers);
 		Map<Integer, Direction> command = new HashMap<Integer, Direction>();
+		command.put(0, Direction.NONE);
+		command.put(1, Direction.NONE);
+		command.put(2, Direction.NONE);
+		command.put(3, Direction.NONE);
 		long timeRound = 1;
 		
 		for(int i = 0 ; i < 1000 ; i++ )
