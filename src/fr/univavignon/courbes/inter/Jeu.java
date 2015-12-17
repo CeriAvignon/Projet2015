5e7b93db624fb60b02b67547faa41a822d71d20d
Nathan Cheval <nathavignon@hotmail.fr>
2015-12-17 14:02:34 +0100

    update

--- src/fr/univavignon/courbes/inter/Jeu.java
+++ src/fr/univavignon/courbes/inter/Jeu.java
@@ -11,20 +11,5 @@ public class Jeu extends JFrame {
 	public Jeu(int largeur, int hauteur) {
 		
 	 }
-	
-	 public void start(){
-		/*  Board board = init(width, height, nbJoueur());
-		  init(board, int pointThreshold, List<Profile> players, terrain, donnee);
-		  long elapsedTime = System.currentTimeMillis();
-		  while(true)
-		  {
-	
-			  
-			  update(elapsedTime, Map<Integer,Direction> commands);
-			  elapsedTime = System.currentTimeMillis() - elapsedTime;
-			  update();
-			  end(); 
-		  }*/
-	 }
 
 }
