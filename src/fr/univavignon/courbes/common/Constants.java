fb3234a2dc1590823a2dc94962363786d7cfaff8
Vincent Labatut <vince.labatut@gmail.com>
2016-01-03 23:07:25 +0100

    mise à jour des constantes

--- src/fr/univavignon/courbes/common/Constants.java
+++ src/fr/univavignon/courbes/common/Constants.java
@@ -9,23 +9,26 @@ package fr.univavignon.courbes.common;
  */
 public class Constants
 {	
-//	/** Vitesse de déplacement normale (i.e. sans l'effet d'un item) des serpents, exprimée en pixel par ms */
-//	public static final float REGULAR_MOVING_SPEED = 1f;
-//	/** Vitesse de déplacement rapide des serpents, exprimée en pixel par ms */
-//	public static final float FAST_MOVING_SPEED = 2f;
-//	/** Vitesse de déplacement lente des serpents, exprimée en pixel par ms */
-//	public static final float SLOW_MOVING_SPEED = 0.5f;
+	/** Vitesse de déplacement normale (i.e. sans l'effet d'un item) des serpents, exprimée en pixel par ms */
+	public static final float REGULAR_MOVING_SPEED = 0.1f;
+	/** Vitesse de déplacement rapide des serpents, exprimée en pixel par ms */
+	public static final float FAST_MOVING_SPEED = 0.1f;	//TODO valeur à estimer
+	/** Vitesse de déplacement lente des serpents, exprimée en pixel par ms */
+	public static final float SLOW_MOVING_SPEED = 0.1f; //TODO valeur à estimer
 	
-//	/** Vitesse normale à laquelle le snake change de direction, exprimée en radians par ms */
-//	public static final float REGULAR_TURNING_SPEED = 1f;
-//	/** Vitesse élevée à laquelle le snake change de direction, exprimée en radians par ms */
-//	public static final float FAST_TURNING_SPEED = 2f;
-//	/** Vitesse lente à laquelle le snake change de direction, exprimée en radians par ms */
-//	public static final float SLOW_TURNING_SPEED = 0.5f;
+	/** Vitesse normale à laquelle le snake change de direction, exprimée en radians par ms */
+	public static final float REGULAR_TURNING_SPEED = 0.003f;
+	/** Vitesse élevée à laquelle le snake change de direction, exprimée en radians par ms */
+	public static final float FAST_TURNING_SPEED = 0.003f; //TODO valeur à estimer
+	/** Vitesse lente à laquelle le snake change de direction, exprimée en radians par ms */
+	public static final float SLOW_TURNING_SPEED = 0.003f; //TODO valeur à estimer
 	
+	/** Rayon de la tête normale du snake, exprimé en pixels */ 
+	public static final int REGULAR_HEAD_RADIUS = 4;
+	/** Rayon de la tête agrandie du snake, exprimé en pixels */ 
+	public static final int LARGE_HEAD_RADIUS = 4; //TODO valeur à estimer
+
 	//TODO différents taux de création de trous ? (taux normal, taux élevé, seulement des trous, autres ?)
-	
-	//TODO rayons de la tête du snake ? (état normal, gros snake, autres ?)
 	
 	// TODO certaines de ces constantes pourraient rendre inutiles des champs de Snake (ex. movingSpeed) dont
 	// la valeur pourrait se déduire de ces constantes et de la nature des items faisant effet sur le Snake au moment considéré.
