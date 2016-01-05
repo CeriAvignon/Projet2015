7f7a4d595d05c9632ea93f3b082253c2eaa59c35
Zakaria LAHRACH <zakaria.lahrach@gmail.com>
2016-01-05 14:21:51 +0100

    ff

--- src/fr/univavignon/courbes/inter/groupe12/error_display.java
+++ src/fr/univavignon/courbes/inter/groupe12/error_display.java
@@ -1,17 +1,24 @@
 package fr.univavignon.courbes.inter.groupe12;
 
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 import fr.univavignon.courbes.inter.ErrorHandler;
 
 /**
  * @author uapv1601921
  *
  */
-public class error_display implements ErrorHandler {
 
-	@Override
-	public void displayError(String errorMessage) {
-		
+		public class error_display extends JFrame implements ErrorHandler {
+
+			@Override
+		    public void displayError(String errorMessage) {
+				
+			JOptionPane.showMessageDialog(this, errorMessage, "erreur!", JOptionPane.WARNING_MESSAGE);
+
+			}
 
 	}
 
-}
+
