c8a07436ceacf597e0dbdbf5f57eae2110c2d11e
Vincent Labatut <vince.labatut@gmail.com>
2016-01-06 18:30:29 +0100

    petite màj javadoc

--- src/fr/univavignon/courbes/common/Snake.java
+++ src/fr/univavignon/courbes/common/Snake.java
@@ -4,9 +4,9 @@ import java.io.Serializable;
 import java.util.Map;
 
 /**
- * Cette classe contient les informations et caractéristiques d'un snake.
+ * Cette classe contient les informations et caractéristiques d'un serpent.
  * <br/>
- * L'ID du joueur associé au snake est relatif à la partie courante. Par 
+ * L'ID du joueur associé au serpent est relatif à la partie courante. Par 
  * exemple, si on a 4 joueurs, alors leurs ID vont de 0 à 3. À ne pas confondre
  * avec l'ID du profile, qui est valable pour le jeu globalement, et indépendant
  * de toute partie.
@@ -15,30 +15,30 @@ public class Snake implements Serializable
 {	/** Numéro de série (pour {@code Serializable}) */
 	private static final long serialVersionUID = 1L;
 	
-	/** Numéro du joueur associé à ce snake, dans la partie en cours */
+	/** Numéro du joueur associé à ce serpent, dans la partie en cours */
 	public int playerId;
-	/** Numéro du profil associé à ce snake, dans la BD du jeu */
+	/** Numéro du profil associé à ce serpent, dans la BD du jeu */
 	public int profileId;
 	
-	/** Position courante de la tête du Snake en abscisse */
+	/** Position courante de la tête du serpent en abscisse */
 	public int currentX;
-	/** Position courante de la tête du Snake en ordonnée */
+	/** Position courante de la tête du serpent en ordonnée */
 	public int currentY;
 	
-	/** Angle représentant la direction de déplacement courante du snake, par rapport à l'horizontale */
+	/** Angle représentant la direction de déplacement courante du serpent, par rapport à l'horizontale */
 	public double currentAngle;
-	/** Rayon de la tête du snake */
+	/** Rayon de la tête du serpent */
 	public double headRadius;
-	/** Vitesse de déplacement du snake, exprimée en pixel par ms */
+	/** Vitesse de déplacement du serpent, exprimée en pixel par ms */
 	public double movingSpeed;
-	/** Vitesse à laquelle le snake change de direction, exprimée en radians par ms */
+	/** Vitesse à laquelle le serpent change de direction, exprimée en radians par ms */
 	public double turningSpeed;
 	
-	/** Etat du snake : {@code false} il est mort, {@code true} il est vivant */
+	/** Etat du serpent : {@code false} il est mort, {@code true} il est vivant */
 	public boolean state;
 	/** Mode de collision : {@code false} il est insensible aux collisions, {@code true} il l’est */
 	public boolean collision;
-	/**Inversion des commandes : {@code false} le snake est dirigé normalement, {@code true} ses commandes sont inversées */
+	/**Inversion des commandes : {@code false} le serpent est dirigé normalement, {@code true} ses commandes sont inversées */
 	public boolean inversion;
 	
 	/** Taux de création de trous dans le tracé : pour 0 on n'a aucun trou, pour 1 on n'a aucune traînée (i.e. que des trous). Ce taux est la probabilité qu'un serpent laisse un trou lors d'une itération donnée. */
@@ -49,9 +49,9 @@ public class Snake implements Serializable
 	/** Mode avion ({@code true}) ou pas ({@code false}) */
 	public boolean fly;
 	
-	/** Items affectant ce snake, associés à leur temps d'effet restant (en ms) */
+	/** Items affectant ce serpent, associés à leur temps d'effet restant (en ms) */
 	public Map<Item, Long> currentItems;
 	
-	/** Score courant du joueur associé au snake dans la partie courante */
+	/** Score courant du joueur associé au serpent dans la partie courante */
 	public int currentScore;
 }
