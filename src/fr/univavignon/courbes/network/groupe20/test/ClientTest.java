2b50cb45bd9549e19b884687f293b7e34b29196b
Ibtissam.Benachour <ben.ibtissam.ib@gmail.com>
2016-01-09 16:50:39 +0100

    Gestion de profils test Finit

--- src/fr/univavignon/courbes/network/groupe20/test/ClientTest.java
+++ src/fr/univavignon/courbes/network/groupe20/test/ClientTest.java
@@ -6,7 +6,10 @@ import fr.univavignon.courbes.common.Profile;
 import fr.univavignon.courbes.inter.ClientProfileHandler;
 import fr.univavignon.courbes.inter.ErrorHandler;
 import fr.univavignon.courbes.network.groupe20.client.Client;
-
+/*
+ * Cette classe permet à l'interface UI de savoir comment les fonctions côter Client fonctionnent
+ *
+ */
 public class ClientTest {
 	public static void main(String[] args) {
 		//Instance de classe ErrorHandler créer par IU
@@ -21,21 +24,26 @@ public class ClientTest {
 		ClientProfileHandler profileHandler = new ClientProfileHandler() {
 			@Override
 			public void updateProfiles(List<Profile> profiles) {
+				System.out.println("=====================Affichage est dans la méthode updateProfiles() dans la classe de type ClientProfileHandler IU==========================");
+				System.out.println("la méthode updateProfiles() : est exsécuté lors de l'envoie des profiles côtés Serveur");
+				System.out.println("les joueurs qui participent : ");
 				for(Profile p : profiles)
 					System.out.println(p.profileId+" "+p.userName);
+				System.out.println("============================================================================================================================================");
 			}
 		};
 		
-		//Instance de la classe client
 		Client c = new Client();
 		//Définition dl'adresse du serveur et le port
 		c.setIp("localhost");c.setPort(1117);
+		c.setErrorHandler(error);
+		c.setProfileHandler(profileHandler);
 		//Lancement du client
 		c.launchClient();
 		//---------------Test du profile début------------->
 		//Création du profil à envoyer
 		Profile p = new Profile();
-		p.profileId = 1;p.userName = "test";
+		p.profileId = 4;p.userName = "test";
 		//-------------------Gestion de profile DÉBUT----------------------//
 		//Ajouter un profile
 		boolean b = c.addProfile(p);
@@ -44,7 +52,8 @@ public class ClientTest {
 		else
 			System.out.println("Profile refuser : "+p.profileId);
 		//Supprimer un profil
-		c.removeProfile(p);
+		//sc.removeProfile(p);
+		
 		//-------------------Gestion de profile FIN------------------------//
 		
 		
--- src/fr/univavignon/courbes/network/groupe20/test/ServerTest.java
+++ src/fr/univavignon/courbes/network/groupe20/test/ServerTest.java
@@ -1,5 +1,6 @@
 package fr.univavignon.courbes.network.groupe20.test;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
@@ -22,7 +23,7 @@ public class ServerTest {
 			List<Profile>  profiles = new CopyOnWriteArrayList<Profile>();
 			@Override
 			public boolean fetchProfile(Profile profile) {
-				if(profiles.size() >= 3){
+				if(profiles.size() < 2){
 					profiles.add(profile);
 					return true;
 				}
@@ -41,8 +42,24 @@ public class ServerTest {
 		//Lancement du serveur pour accepter des clients
 		s.launchServer();
 		
-		
-		
+		//-------------------Gestion de profile DÉBUT----------------------//
+		//Création des profiles qui sont dans le serveur
+		List<Profile> profiles = new ArrayList<>();
+		Profile p1 = new Profile();
+		p1.profileId = 0;p1.userName = "serveurProfile1";
+		Profile p2 = new Profile();
+		p2.profileId = 1;p2.userName = "serveurProfile2";
+		//Ajout des profiles à la list qu'on a envoyé
+		profiles.add(p1);profiles.add(p2);
+		//Envoie de la list des profiles du serveur à tous les clients connéctés
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		s.sendProfiles(profiles);
+		System.out.println("envoyer");
 		
 		
 		
