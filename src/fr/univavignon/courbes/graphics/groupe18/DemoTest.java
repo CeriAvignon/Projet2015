6f77c5e68791e1e4ce9d38746757ab125b094b6c
Axel Clerici <axel.clerici@gmail.com>
2016-01-13 22:22:56 +0100

    commit

--- src/fr/univavignon/courbes/graphics/groupe18/DemoTest.java
+++ src/fr/univavignon/courbes/graphics/groupe18/DemoTest.java
@@ -2,6 +2,7 @@ package fr.univavignon.courbes.graphics.groupe18;
 
 public class DemoTest {
 	public static void main(){
-		
+		// Un joueur, Q et D pour diriger.
+		(new MinimalLoop()).mainLoop(1);
 	}
 }
--- src/fr/univavignon/courbes/graphics/groupe18/RegularTest.java
+++ src/fr/univavignon/courbes/graphics/groupe18/RegularTest.java
@@ -2,6 +2,8 @@ package fr.univavignon.courbes.graphics.groupe18;
 
 public class RegularTest {
 	public static void main(String[] args) {
+		// Deux Joueurs, Q et D pour diriger le premier,
+		// K et M pour diriger le second
 		(new MinimalLoop()).mainLoop(2);
 	}
 }
