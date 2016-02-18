fc22be6128e7d1163e16dd598fe2af1705218b68
Vincent Labatut <vince.labatut@gmail.com>
2016-02-18 14:27:19 +0100

    Correction issue #7

--- src/fr/univavignon/courbes/inter/simpleimpl/profiles/ProfileListPanel.java
+++ src/fr/univavignon/courbes/inter/simpleimpl/profiles/ProfileListPanel.java
@@ -24,7 +24,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
-import java.util.Collections;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
